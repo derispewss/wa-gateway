7af97cf83bf504328514d90ceb8bfa9ae97307ea
derispewss <derisfirmansyah177@gmail.com>
2024-06-20 16:24:08 +0700

    push

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set("view engine", "ejs");
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // Public Path
 app.use("/p", express.static(path.resolve("public")));
